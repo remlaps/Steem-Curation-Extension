12e9ca0f369367f097409f9856699e65a5bf23f1
remlaps <remlaps@aol.com>
2024-05-24 17:44:47 -0400

    BUG FIX: Get highlighting to start without a mouse click or scroll action.

--- main.js
+++ main.js
@@ -1,65 +1,44 @@
 console.log("The extension is up and running");
-var promotedPosts = {}; // contains all transactions for promoted posts with accounts, count and whether self promoted
+
+var promotedPosts = {}; // contains all transactions for promoted posts with accounts, count, and whether self-promoted
 const urlRequest = "https://sds.steemworld.org/transfers_api/getTransfersByTypeTo/transfer/null/time/DESC/250/0";
 
-/*
-// functions:
-// - highLight - Changes background color according to size of @null beneficiary or promotion amount
-// - getColorBurnPost - Takes beneficiary % as an argument and returns color gradient for posts with @null beneficiaries and no post promotion
-// - getColorPromotedPost - Takes promotion cost as an argument and returns color gradient for posts with post promotion but not @null beneficiary
-// - prepareData - takes list of transfers as argument, returns number of total promotions and promotions by author
-// - getAuthorPost - takes a memo string as an argument, returns the author of a post
-// - addText - takes a promoted post object as an argument from the web page and adds the number of promotions and whether by self.
-// - getPost - takes an address as a target, returns [author]/[permlink] if matched, otherwise returns null.
-// - regexMatch - Checks whether a web page post object matches a transfer memo and returns true/false.
-// - getAddress - takes a promoted post document object as an argument, returns the URL of a Steemit post
-*/ 
 const highLight = () => {
     var curatorBackgroundColor;
     const listItem = document.querySelectorAll('li');
 
     // Working from high to low (for outside to inside in document nesting)
-    for (let i=listItem.length-1; i>=0; i--) {
-
+    for (let i = listItem.length - 1; i >= 0; i--) {
         // Check for @null beneficiary and /promoted post promotion.
-        if ( listItem[i].textContent.match('null: .*%' ) && listItem[i].textContent.match('Promotion Cost .*\$') ) {
+        if (listItem[i].textContent.match('null: .*%') && listItem[i].textContent.match('Promotion Cost .*\$')) {
             console.log("Found a /promoted post in #burnsteem25 (outer block)");
             curatorBackgroundColor = '#1E90FF';
             listItem[i].style['background-color'] = curatorBackgroundColor;
 
         // Check for just @null beneficiary
-        } else if ( listItem[i].textContent.match('null: .*%' )) {
-            // console.log("#burnsteem25 outer match: ");
-            if ( listItem[i].textContent.match('^null:.*\%') ) {
-                console.log("Found #burnsteem25");
-                var str = listItem[i].textContent;
-                var nullPct = str.substring(
-                    str.indexOf(" ") + 1,
-                    str.lastIndexOf("%")
-                );
-                curatorBackgroundColor = getColorBurnPost (nullPct);
-            }
+        } else if (listItem[i].textContent.match('null: .*%')) {
+            console.log("Found #burnsteem25");
+            var str = listItem[i].textContent;
+            var nullPct = str.substring(
+                str.indexOf(" ") + 1,
+                str.lastIndexOf("%")
+            );
+            curatorBackgroundColor = getColorBurnPost(nullPct);
             listItem[i].style['background-color'] = curatorBackgroundColor;
 
         // Check for just /promoted post promotion
-        } else if ( listItem[i].textContent.match('Promotion Cost .*\$') ) {
-            // console.log("Found a /promoted post (outer block)");
+        } else if (listItem[i].textContent.match('Promotion Cost .*\$')) {
+            console.log("Found a /promoted post");
+            var str = listItem[i].textContent;
+            var indexEnd = (str.indexOf("(") >= 0) ? str.indexOf("(") - 1 : str.length;
+            var promoAmount = str.substring(
+                str.indexOf("$") + 1,
+                indexEnd
+            );
+            curatorBackgroundColor = getColorPromotedPost(promoAmount);
 
-            if ( listItem[i].textContent.match('^Promotion Cost .*\$$') ) {
-                console.log("Found a /promoted post");
-                var str = listItem[i].textContent;
-                var indexEnd = (str.indexOf("(") >= 0) ? str.indexOf("(") - 1 : str.length;
-                var promoAmount = str.substring(
-                    str.indexOf("$") + 1,
-                    indexEnd
-                );
-                // console.log ("Promotion amount: " + promoAmount);
-
-                curatorBackgroundColor = getColorPromotedPost ( promoAmount );
-
-                // now edit the textContent
-                addText(listItem[i]);
-            }
+            // now edit the textContent
+            addText(listItem[i]);
             listItem[i].style['background-color'] = curatorBackgroundColor;
         } else {
             listItem[i].style['background-color'] = "initial";
@@ -67,42 +46,39 @@ const highLight = () => {
     }
 }
 
-function getColorBurnPost ( nullPct) {
-
-    if ( nullPct > 0 && nullPct < 25 ) {
+function getColorBurnPost(nullPct) {
+    if (nullPct > 0 && nullPct < 25) {
         curatorBackgroundColor = "coral";
-    } else if ( nullPct < 50 ) {
+    } else if (nullPct < 50) {
         curatorBackgroundColor = "orange";
-    } else if ( nullPct < 75 ) {
+    } else if (nullPct < 75) {
         curatorBackgroundColor = "darkorange";
-    } else if ( nullPct > 0 ) {
+    } else if (nullPct > 0) {
         curatorBackgroundColor = "orangered";
     }
     return curatorBackgroundColor;
 }
 
-function getColorPromotedPost ( promoAmount ) {
-    if ( promoAmount > 0 && promoAmount < 0.26 ) {
+function getColorPromotedPost(promoAmount) {
+    if (promoAmount > 0 && promoAmount < 0.26) {
         curatorBackgroundColor = "paleturquoise";
-    } else if ( promoAmount < 0.51 ) {
+    } else if (promoAmount < 0.51) {
         curatorBackgroundColor = "aquamarine";
-    } else if ( promoAmount < 1.01 ) {
+    } else if (promoAmount < 1.01) {
         curatorBackgroundColor = "turquoise";
-    } else if ( promoAmount > 0 ) {
+    } else if (promoAmount > 0) {
         curatorBackgroundColor = "lightseagreen";
     }
     return curatorBackgroundColor;
 }
 
-// load transfers to null and prepare promotedPosts for further use
-fetch (urlRequest).then (function (response) {
+fetch(urlRequest).then(function (response) {
     return response.json();
-}).then (function (data) {
+}).then(function (data) {
     prepareData(data);
-    // execute hihgLight after providing the data
     highLight();
-}).catch (function (error) {
-    console.log ("error: " + error);
+}).catch(function (error) {
+    console.log("error: " + error);
 });
 
 function prepareData(data) {
@@ -113,12 +89,12 @@ function prepareData(data) {
             let trfData = getAuthorPost(trf[cols["memo"]]);
             if (trfData) {
                 let from = trf[cols["from"]];
-                let self = (from == trfData["author"])
-                let props = {"user": [from], "count": 1, "self": self};
+                let self = (from == trfData["author"]);
+                let props = { "user": [from], "count": 1, "self": self };
                 let key = trfData["post"];
                 if (promotedPosts && key in promotedPosts) {
                     let oldProps = promotedPosts[key];
-                    if ( !(oldProps["user"].includes(from)) ) {
+                    if (!(oldProps["user"].includes(from))) {
                         props["user"] = props["user"].concat(oldProps["user"]);
                         props["count"] += oldProps["count"];
                         props["self"] = props["self"] || oldProps["self"];
@@ -128,44 +104,37 @@ function prepareData(data) {
                 }
                 promotedPosts[key] = props;
             }
-        }); 
+        });
     };
 }
 
 function getAuthorPost(memoStr) {
-    // const re = /^@(?<author>[\w-.]+)[\/](?<permlink>[\w-\|]+)$/;
-    // const objMatch = memoStr.match(re);
     const objMatch = regexMatch(true, memoStr);
     let result = (objMatch && objMatch.length == 3) ? {
         "post": objMatch.groups["author"] + "/" + objMatch.groups["permlink"],
         "author": objMatch.groups["author"]
-        } : null ;
+    } : null;
     return result;
 }
 
 function addText(listItem) {
     var added = false;
-    // console.log("include User? " + listItem.textContent.includes('User'));
-    if ( !listItem.textContent.includes('User') ) {
-    // get the postid
+    if (!listItem.textContent.includes('User')) {
         let address = getAddress(listItem);
-        console.log("Address: " + address);
-        if ( address !== null ) {
+        if (address !== null) {
             let key = getPost(address);
-            // console.log("Key: " + key);
-            // console.log("Promoted: " + promotedPosts[key]);
-            if ( promotedPosts[key] ) {
-                let newText = ' (by ' + 
-                    promotedPosts[key]["count"] + 
-                    ( promotedPosts[key]["count"] == 1 ? ' User' : ' Users' ) + 
-                    ( promotedPosts[key]["self"] ? ' incl. self)' : ')' );
+            if (promotedPosts[key]) {
+                let newText = ' (by ' +
+                    promotedPosts[key]["count"] +
+                    (promotedPosts[key]["count"] == 1 ? ' User' : ' Users') +
+                    (promotedPosts[key]["self"] ? ' incl. self)' : ')');
                 let newTextNode = document.createTextNode(newText);
                 listItem.firstChild.appendChild(newTextNode);
                 added = true
             }
-        }        
+        }
     }
-    if ( added ) {
+    if (added) {
         console.log("User added");
     } else {
         console.log("Adding User went wrong");
@@ -184,9 +153,9 @@ function regexMatch(fromBegin, textStr) {
 
 function getAddress(elem) {
     var link;
-    while ( elem.parentElement && elem.parentElement.nodeName.toLowerCase() != 'body' ) {
+    while (elem.parentElement && elem.parentElement.nodeName.toLowerCase() != 'body') {
         elem = elem.parentElement;
-        if ( elem.nodeName.toLowerCase() == 'div' && elem.className.includes('articles__content-block--text') ) {
+        if (elem.nodeName.toLowerCase() == 'div' && elem.className.includes('articles__content-block--text')) {
             let titleElemList = elem.getElementsByClassName('entry-title');
             link = titleElemList[0].firstChild.href;
             break;
@@ -195,8 +164,9 @@ function getAddress(elem) {
     return link ? link : null;
 }
 
-highLight();
-
+document.addEventListener('DOMContentLoaded', () => {
+    highLight();
+});
 
 window.addEventListener('scroll', () => {
     highLight();
@@ -204,14 +174,8 @@ window.addEventListener('scroll', () => {
 window.addEventListener('load', () => {
     highLight();
 });
-
 window.addEventListener('click', () => {
-	highLight();
-});
-
-window.addEventListener('DOMContentLoaded', 
-() => {
-	highLight();
+    highLight();
 });
 
 console.log("The extension is done.");
